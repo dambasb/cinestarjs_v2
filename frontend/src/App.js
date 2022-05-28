2dc26c0d96649334d19f9fe5a4ce20f0700e9024
Damir Kolobaric <dkolobaric@gmail.com>
2022-05-28 14:08:50 +0200

    Set Login Route
    Set Login Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen'
 import ActionScreen from './screens/ActionScreen'
 import ComedyScreen from './screens/ComedyScreen'
 import ScienceFictionScreen from './screens/ScienceFictionScreen'
 import Sidebar from './components/Sidebar'
+import AuthenticationScreen from './screens/AuthenticationScreen'
 
 const App = () => {
+  const url = window.location.href
+  const [isAuthentication, setIsAuthentication] = useState(false)
+
+  useEffect(() => {
+    if (('login' || 'registration') === url.split('/')[3]) {
+      console.log(url.split('/')[3])
+      setIsAuthentication(true)
+    }
+  }, [])
+
+  console.log(isAuthentication)
+
   return (
     <Router>
-      <div className='has-sidebar'>
-        <div className='sidebar'>
-          <Sidebar />
+      {!isAuthentication && (
+        <div className='has-sidebar'>
+          <div className='sidebar'>
+            <Sidebar />
+          </div>
+          <div className='screens'>
+            <Routes>
+              <Route path='/' element={<HomeScreen />} exact />
+              <Route path='/action' element={<ActionScreen />} exact />
+              <Route path='/comedy' element={<ComedyScreen />} exact />
+              <Route
+                path='/science-fiction'
+                element={<ScienceFictionScreen />}
+                exact
+              />
+            </Routes>
+          </div>
         </div>
-        <div className='screens'>
-          <Routes>
-            <Route path='/' element={<HomeScreen />} exact />
-            <Route path='/action' element={<ActionScreen />} exact />
-            <Route path='/comedy' element={<ComedyScreen />} exact />
-            <Route
-              path='/science-fiction'
-              element={<ScienceFictionScreen />}
-              exact
-            />
-          </Routes>
-        </div>
-      </div>
+      )}
+      <Routes>
+        <Route path='/login' element={<AuthenticationScreen />} exact />
+        <Route path='/registration' element={<AuthenticationScreen />} exact />
+      </Routes>
     </Router>
   )
 }
---
+++ frontend/src/screens/AuthenticationScreen.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AuthenticationScreen = (props) => {
+  return <div>AuthenticationScreen</div>
+}
+
+export default AuthenticationScreen
